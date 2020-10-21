cc57a29b59cf67486b8d63a28c045118c0fd980a
tutur0001 <a.michon@it-students.fr>
2020-10-21 13:43:52 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react' 
 import './App.css';
+import Membre from './components/Membre'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+  render() {
+    return(
+      <Fragment>
+        <div className='App'>
+          <h1>First App in react</h1>
+          <Membre />
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Membre.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+// Component Stateless
+
+const Membre = () => {
+    return (
+        <h2>Membre</h2>
+    )
+}
+
+// 
+
+
+
+
+
+
+export default Membre
